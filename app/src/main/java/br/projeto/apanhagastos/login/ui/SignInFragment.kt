88b4252c51b9f9a58be6cf71fe716708ee0bf39a
ThiagoBianchini <thiago.sa@al.infnet.edu.br>
2022-11-16 08:59:21 -0300

    nav entre telas feita, problema do tema escuro, problema de escala da tela

--- app/src/main/java/br/projeto/apanhagastos/login/ui/SignInFragment.kt
+++ app/src/main/java/br/projeto/apanhagastos/login/ui/SignInFragment.kt
@@ -5,17 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.projeto.apanhagastos.R
+import br.projeto.apanhagastos.databinding.FragmentSignInBinding
 
 
 class SignInFragment : Fragment() {
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        setup()
+        return view
     }
 
+    private fun setup() {
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.apply {
+            tvNovaConta.setOnClickListener {
+                nav(R.id.action_signInFragment_to_signOnFragment)
+            }
+        }
+    }
+
+    fun nav(id: Int) {
+        findNavController().navigate(id)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/br/projeto/apanhagastos/login/ui/SignOnFragment.kt
+++ app/src/main/java/br/projeto/apanhagastos/login/ui/SignOnFragment.kt
@@ -5,17 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.projeto.apanhagastos.R
+import br.projeto.apanhagastos.databinding.FragmentSignOnBinding
 
 
 class SignOnFragment : Fragment() {
+    private var _binding: FragmentSignOnBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_on, container, false)
+        _binding = FragmentSignOnBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        setup()
+        return view
     }
 
+    private fun setup() {
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.apply {
+            tvTemConta.setOnClickListener {
+                nav(R.id.action_signOnFragment_to_signInFragment)
+            }
+        }
+    }
+
+    fun nav(id: Int) {
+        findNavController().navigate(id)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
