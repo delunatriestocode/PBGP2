f90f786150492c2f0c61a828a492c50dfe41bd3d
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-11-18 10:29:11 -0300

    Configuração do Repositorio Gastos

--- app/src/main/java/br/projeto/apanhagastos/models/Gastos.kt
+++ app/src/main/java/br/projeto/apanhagastos/models/Gastos.kt
@@ -1,3 +1,7 @@
 package br.projeto.apanhagastos.models
 
-data class Gastos (val name: String, val categoria: String, val custo: Long )
+data class Gastos (
+    val name: String = "",
+    val categoria: String = "",
+    val custo: Long = 0L,
+)
