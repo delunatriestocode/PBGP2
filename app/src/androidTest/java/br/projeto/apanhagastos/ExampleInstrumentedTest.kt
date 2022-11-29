d9dd12fb1be1cb344f4ecd2819e6bd9ea6628ed2
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-11-29 12:25:56 -0300

    1234

--- app/src/androidTest/java/br/projeto/apanhagastos/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/projeto/apanhagastos/ExampleInstrumentedTest.kt
@@ -2,11 +2,13 @@ package br.projeto.apanhagastos
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,7 +16,14 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+    @Rule
+    // val activityRule = ActivityTestRule(MyClass::class.java)
+
+
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
